27af60f11ffbdfa20c0bbd4af509ef95910cd9e4
Dmitry Sharov <dmitry.sharov91@gmail.com>
2018-12-05 21:16:00 +0200

    init commit

---
+++ src/test/java/mcpp/mcppclas.java
@@ -0,0 +1,13 @@
+package mcpp;
+import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class mcppclas {
+    @Test
+    public void openGoogle() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+    }
+}
